92c301e67d7583a7755088b6fd2b7bc26d998f81
Mateus Sousa do Carmo <mateus.1090ptbr0@gmail.com>
2025-05-10 10:58:10 -0300

    add host

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ def home():
 def dash1():
     return redirect('/home/dash1/')
 
-if __name__ == '__main__': app.run(debug=True)
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', debug=True)
     
 
